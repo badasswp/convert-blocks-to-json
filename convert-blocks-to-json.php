0a8b35b2d8fcc8e189273f2ec531362218ad93d7
badasswp <badasswpdev@gmail.com>
2024-08-22 22:00:33 +0100

    chore: set up plugin entry file

---
+++ convert-blocks-to-json.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Convert Blocks to JSON
+ * Plugin URI:  https://github.com/badasswp/convert-blocks-to-json
+ * Description: Convert your WP blocks to JSON.
+ * Version:     1.0.0
+ * Author:      badasswp
+ * Author URI:  https://github.com/badasswp
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: convert-blocks-to-json
+ * Domain Path: /languages
+ *
+ * @package ConvertBlocksToJSON
+ */
+
+namespace badasswp\ConvertBlocksToJSON;
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
