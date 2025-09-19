61f15d97fd75fa7ff811dcf09be44b2b441c9d74
badasswp <badasswpdev@gmail.com>
2025-09-19 20:15:45 +0100

    refactor: routes logic using classes

---
+++ inc/Abstracts/Route.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Route abstraction.
+ *
+ * This abstract class defines a foundation for creating
+ * route classes which act as WP REST end points.
+ *
+ * @package ConvertBlocksToJSON
+ */
+
+namespace ConvertBlocksToJSON\Abstracts;
+
+use ConvertBlocksToJSON\Interfaces\Router;
+
+/**
+ * Route class.
+ */
+abstract class Route implements Router {
+	/**
+	 * Get, Post, Put, Patch, Delete.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @var string
+	 */
+	public string $method;
+
+	/**
+	 * REST Callback.
+	 *
+	 * Also known as the Request Callback. This method is
+	 * responsible for getting the $request data and passing it along
+	 * to the response method.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param \WP_REST_Request $request Request object.
+	 * @return \WP_REST_Response|\WP_Error
+	 */
+	abstract public function rest_callback( $request );
+
+	/**
+	 * Permission Callback.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @return string|array
+	 */
+	public function get_permission_callback() {
+		if ( $this->method === \WP_REST_Server::READABLE ) {
+			return '__return_true';
+		}
+
+		return [ $this, 'is_user_permissible' ];
+	}
+
+	/**
+	 * Register REST Route.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function register_route(): void {
+		/**
+		 * Filter REST namespace.
+		 *
+		 * Provide users the ability to customize the
+		 * default REST namespace.
+		 *
+		 * @since 1.1.0
+		 *
+		 * @return string
+		 */
+		$rest_namespace = (string) apply_filters( 'cbtj_rest_namespace', 'cbtj/v1' );
+
+		register_rest_route(
+			$rest_namespace,
+			'/(?P<id>\d+)',
+			[
+				'methods'             => $this->method,
+				'callback'            => [ $this, 'rest_callback' ],
+				'permission_callback' => $this->get_permission_callback(),
+			]
+		);
+	}
+
+	/**
+	 * Get 400 Response.
+	 *
+	 * This method returns a 400 response for Bad
+	 * requests submitted.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param string $message Error Msg.
+	 * @return \WP_Error
+	 */
+	public function get_400_response( $message ): \WP_Error {
+		$args = $this->request->get_json_params();
+
+		return new \WP_Error(
+			'cbtj-bad-request',
+			sprintf(
+				'Fatal Error: Bad Request, %s',
+				$message
+			),
+			[
+				'status'  => 400,
+				'request' => $args,
+			]
+		);
+	}
+
+	/**
+	 * Is User Permissible?
+	 *
+	 * Validate that User has Admin capabilities
+	 * and Nonce is set correctly.
+	 *
+	 * @since 1.0.2
+	 *
+	 * @wp-hook 'rest_api_init'
+	 *
+	 * @param \WP_REST_Request $request Request Object.
+	 * @return bool|\WP_Error
+	 */
+	public function is_user_permissible( $request ) {
+		$http_error = rest_authorization_required_code();
+
+		if ( ! current_user_can( 'administrator' ) ) {
+			return new \WP_Error(
+				'cbtj-rest-forbidden',
+				sprintf( 'Invalid User. Error: %s', $http_error ),
+				[ 'status' => $http_error ]
+			);
+		}
+
+		if ( ! wp_verify_nonce( $request->get_header( 'X-WP-Nonce' ), 'wp_rest' ) ) {
+			return new \WP_Error(
+				'cbtj-rest-forbidden',
+				sprintf( 'Invalid Nonce. Error: %s', $http_error ),
+				[ 'status' => $http_error ]
+			);
+		}
+
+		return true;
+	}
+}
---
+++ inc/Routes/Import.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Import Service.
+ *
+ * Handle Import REST route registration
+ * & WP hook bindings.
+ *
+ * @package ConvertBlocksToJSON
+ */
+
+namespace ConvertBlocksToJSON\Services;
+
+use ConvertBlocksToJSON\Abstracts\Route;
+use ConvertBlocksToJSON\Interfaces\Router;
+
+/**
+ * Import class.
+ */
+class Import extends Route implements Router {
+	/**
+	 * Get, Post, Put, Patch, Delete.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @var string
+	 */
+	public string $method = \WP_REST_Server::CREATABLE;
+
+	/**
+	 * Get REST Response.
+	 *
+	 * This method grabs the JSON attachment
+	 * that has been imported.
+	 *
+	 * @since 1.0.1
+	 *
+	 * @wp-hook 'rest_api_init'
+	 *
+	 * @param \WP_REST_Request $request Request Object.
+	 * @return \WP_REST_Response|\WP_Error
+	 */
+	public function rest_callback( $request ) {
+		$args = $request->get_json_params();
+
+		// Get Post ID & JSON file.
+		$post_id   = (int) ( $args['id'] ?? '' );
+		$json_file = get_attached_file( $post_id );
+
+		// Bail out, if it does NOT exists.
+		if ( ! file_exists( $json_file ) ) {
+			return $this->get_400_response(
+				sprintf(
+					'File does not exists for ID: %s',
+					$post_id
+				)
+			);
+		}
+
+		// Bail out, if it is not JSON.
+		if ( 'json' !== wp_check_filetype( $json_file )['ext'] ?? '' ) {
+			return $this->get_400_response(
+				sprintf(
+					'Fatal Error: Wrong file type: %s',
+					$args['filename'] ?? ''
+				)
+			);
+		}
+
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+		$json   = file_get_contents( $json_file );
+		$import = $this->get_blocks_import( json_decode( $json, true ), $post_id );
+
+		return rest_ensure_response( $import );
+	}
+
+	/**
+	 * Get Blocks Import.
+	 *
+	 * Loop through the JSON array of blocks
+	 * and render as string.
+	 *
+	 * @param 1.0.1
+	 *
+	 * @param array   $json    JSON Array of Blocks.
+	 * @param integer $post_id Post ID.
+	 *
+	 * @return mixed[]
+	 */
+	public function get_blocks_import( $json, $post_id ): array {
+		$import = array_map(
+			[ $this, 'get_import' ],
+			$json['content'] ?? []
+		);
+
+		/**
+		 * Filter JSON Import.
+		 *
+		 * @since 1.0.1
+		 *
+		 * @param mixed[] $response Import Blocks.
+		 * @param integer $post_id  Post ID.
+		 *
+		 * @return mixed[]
+		 */
+		return (array) apply_filters( 'cbtj_rest_import', $import, $post_id );
+	}
+
+	/**
+	 * Get Import Content.
+	 *
+	 * Loop through the JSON blocks and format
+	 * correctly for use on JS.
+	 *
+	 * @since 1.0.1
+	 *
+	 * @param mixed[] $block Block array.
+	 * @return mixed[]
+	 */
+	public function get_import( $block ): array {
+		$children = [];
+
+		if ( ! empty( $block['innerBlocks'] ) ) {
+			foreach ( $block['innerBlocks'] as $child_block ) {
+				$children[] = $this->get_import( $child_block );
+			}
+		}
+
+		$block['attributes']['content'] = $block['filtered'] ?? '';
+
+		return [
+			'name'        => $block['name'] ?? '',
+			'attributes'  => wp_json_encode( $block['attributes'] ?? [] ),
+			'innerBlocks' => $children ?? [],
+		];
+	}
+}
