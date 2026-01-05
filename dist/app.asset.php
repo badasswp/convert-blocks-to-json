7cd5197801caa5078794f5da633d9ecc99aede16
badasswp <badasswpdev@gmail.com>
2026-01-05 04:45:12 +0000

    push to master-built

---
+++ dist/app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins'), 'version' => 'af0a14b1e3ec484454e6');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/convert-blocks-to-json',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '589e8efe8f85d146472d4dea349a9d7a265e9c07',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/convert-blocks-to-json' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '589e8efe8f85d146472d4dea349a9d7a265e9c07',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
