547f09c57b9d1c415b05267b05d1459334e91ef3
badasswp <badasswpdev@gmail.com>
2026-02-11 07:11:20 +0100

    fix: wp deprecation warnings

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { __ } from '@wordpress/i18n';
 import { Fragment } from '@wordpress/element';
 import { PanelBody } from '@wordpress/components';
 import { registerPlugin } from '@wordpress/plugins';
-import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
+import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/editor';
 
 import ViewJSON from './components/ViewJSON';
 import ImportJSON from './components/ImportJSON';
