0bf334b635ad1f34ba789a6249191d574375b69e
badasswp <badasswpdev@gmail.com>
2026-02-15 19:00:14 +0100

    test: update unit tests

--- tests/unit/php/Services/BlocksTest.php
+++ tests/unit/php/Services/BlocksTest.php
@@ -14,6 +14,7 @@ use ConvertBlocksToJSON\Blocks\Lists;
 use ConvertBlocksToJSON\Blocks\ListItem;
 use ConvertBlocksToJSON\Blocks\Image;
 use ConvertBlocksToJSON\Blocks\Paragraph;
+use ConvertBlocksToJSON\Blocks\Table;
 
 /**
  * @covers \ConvertBlocksToJSON\Services\Blocks::__construct
@@ -43,6 +44,7 @@ class BlocksTest extends WPMockTestCase {
 				ListItem::class,
 				Image::class,
 				Paragraph::class,
+				Table::class,
 			],
 			$this->blocks->blocks
 		);
@@ -86,6 +88,7 @@ class BlocksTest extends WPMockTestCase {
 				ListItem::class,
 				Image::class,
 				Paragraph::class,
+				Table::class,
 			]
 		);
 
