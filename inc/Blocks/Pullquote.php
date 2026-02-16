31e8216ee6f8044c4cb43f7e1b2b766701e124e6
activist507 <96887053+activist507@users.noreply.github.com>
2026-02-16 21:15:48 +0100

    Update inc/Blocks/Pullquote.php
    Co-authored-by: badasswp <badasswpdev@gmail.com>

--- inc/Blocks/Pullquote.php
+++ inc/Blocks/Pullquote.php
@@ -22,7 +22,7 @@ class Pullquote extends Block {
 	 * @return mixed[]
 	 */
 	public function import_block( $block ): array {
-		//Bail out, if undefined or not Pullquote block
+		// Bail out, if undefined or not Pullquote block.
 		if ( empty( $block['name'] ) || 'core/pullquote' !== $block['name'] ) {
 			return $block;
 		}
