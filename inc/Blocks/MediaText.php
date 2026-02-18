51579167b526cb637c79f09125b5de9147e28d22
activist507 <96887053+activist507@users.noreply.github.com>
2026-02-18 14:14:33 +0100

    Update inc/Blocks/MediaText.php
    Co-authored-by: badasswp <badasswpdev@gmail.com>

--- inc/Blocks/MediaText.php
+++ inc/Blocks/MediaText.php
@@ -63,7 +63,7 @@ class MediaText extends Block {
 	 * @return mixed[]
 	 */
 	public function export_block( $block ): array {
-		//Bail out, if undefined OR not MediaText block.
+		// Bail out, if undefined OR not MediaText block.
 		if ( empty( $block['name'] ) || 'core/media-text' !== $block['name'] ) {
 			return $block;
 		}
