ee0bd3e4d2e7ec2e2c9a23a0596eac1539f691b4
badasswp <badasswpdev@gmail.com>
2026-02-27 04:13:57 +0000

    Build from release-1.3.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/convert-blocks-to-json',
+        'pretty_version' => 'dev-release-1.3.0-built',
+        'version' => 'dev-release-1.3.0-built',
+        'reference' => 'ac9799163b9ab9a559226f806558c530b1372ddd',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/convert-blocks-to-json' => array(
+            'pretty_version' => 'dev-release-1.3.0-built',
+            'version' => 'dev-release-1.3.0-built',
+            'reference' => 'ac9799163b9ab9a559226f806558c530b1372ddd',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
