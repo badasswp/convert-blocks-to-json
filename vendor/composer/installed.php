1823ce1a7fd23f04f06c31cfc2bd6f29dc9ecbf5
badasswp <badasswpdev@gmail.com>
2026-05-25 12:27:47 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/convert-blocks-to-json',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1fc46c61e32fcc190d4a9a98fe5790e27532e2ee',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/convert-blocks-to-json' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1fc46c61e32fcc190d4a9a98fe5790e27532e2ee',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
