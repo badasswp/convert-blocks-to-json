84e872fd83d712f62f6f8f86f3788a815846f7f4
badasswp <badasswpdev@gmail.com>
2026-06-03 10:39:33 +0000

    Build from release-1.4.0

---
+++ dist/app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins'), 'version' => '6511a9115045e6bbbb7a');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/convert-blocks-to-json',
+        'pretty_version' => 'dev-release-1.4.0-built',
+        'version' => 'dev-release-1.4.0-built',
+        'reference' => '1fc46c61e32fcc190d4a9a98fe5790e27532e2ee',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/convert-blocks-to-json' => array(
+            'pretty_version' => 'dev-release-1.4.0-built',
+            'version' => 'dev-release-1.4.0-built',
+            'reference' => '1fc46c61e32fcc190d4a9a98fe5790e27532e2ee',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
